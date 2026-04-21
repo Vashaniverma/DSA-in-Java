4b51c7cd40bd493149ba81659408abc614051528
Vashani verma <144155899+Vashaniverma@users.noreply.github.com>
2026-04-21 15:26:23 +0530

    Add files via upload

---
+++ inverted_half_pyramid.java
@@ -0,0 +1,15 @@
+public class inverted_half_pyramid {
+    public static void main(String[] args) {
+        for(int i=5;i>=1;i--){
+            for(int j=1;j<=5;j++){
+                if(i>=j){
+                    System.err.print(" "+j);
+                }
+                else{
+                    System.err.print("  ");
+                }
+            }
+            System.err.println("");
+        }
+    }
+}
