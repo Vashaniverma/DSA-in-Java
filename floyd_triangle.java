05c748bfdf7a8f110ad4f2b1c2ee8ddd05bf6bfb
Vashani verma <144155899+Vashaniverma@users.noreply.github.com>
2026-04-21 15:34:22 +0530

    Add files via upload

---
+++ floyd_triangle.java
@@ -0,0 +1,14 @@
+public class floyd_triangle {
+    public static void main(String[] args) {
+        int k=1;
+        for(int i=1;i<=5;i++){
+            for(int j=1; j<=5;j++){
+                if(j<=i){
+                    System.err.print(" "+k);
+                    k++;
+                }    
+            }
+            System.err.println("");
+        }
+    }
+}
